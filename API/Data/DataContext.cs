8fe231e25b0da9ce4811090401b72363553054f0
Sergey Anikanov <anikanovs@yandex.ru>
2024-07-22 01:29:28 +0300

    Entity Customer added

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,4 +7,5 @@ public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<AppUser> Users { get; set; }
     public DbSet<OrderPoint> OrderPoints { get; set; }
+    public DbSet<Customer> Customers { get; set; }
 }
---
+++ API/Entities/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Entities;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public string Patronymic { get; set; } = string.Empty;
+    public required string Phone { get; set; }
+    public string Passport { get; set; } = string.Empty;
+}
